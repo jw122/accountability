96f4dc66eed0ce5ecaca79f8d0925390f86e4d8c
jw122 <thejuliawu@gmail.com>
2017-12-18 00:10:55 -0800

    adding preliminary contract variables and functions

--- contracts/Accountability.sol
+++ contracts/Accountability.sol
@@ -1,10 +1,23 @@
 pragma solidity ^0.4.4;
 
 contract Accountability {
-  function Accountability() {
+
+  address public originator; // address of the individual being held accountable
+  address public evilOrg; // address of the recipient of funds if goal not accomplished
+  uint public balance; // amount pledged for goal
+
+  function Accountability(address _evilOrg) {
     // constructor
+    originator = msg.sender;
+    evilOrg = _evilOrg;
   }
 
+  /* check if this works */
+  function refundOriginator(){
+    originator.send(balance);
+  }
+
+  /* Function that enables the contract to receive funds */
   function () payable{
 
   }
